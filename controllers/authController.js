9d7cce2b73ff87405964ee2046f2574f9cef4eeb
Aman <amangarg0303@gmail.com>
2024-04-22 00:07:15 +0530

    send jwt as cookie token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,12 @@
 import User from "../models/user.js";
 import createError from "../utils/createError.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// Generate a token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
 export const register = async (req, res, next) => {
   try {
@@ -31,8 +37,22 @@ export const register = async (req, res, next) => {
       password: hashedPass,
     });
 
-    // console.log(newUser);
-    res.status(201).json(newUser);
+    const token = generateToken(newUser?._id);
+    if (newUser) {
+      const { password, ...details } = newUser._doc;
+      res
+        .cookie("token", token, {
+          path: "/",
+          httpOnly: true,
+          expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 1 day
+          sameSite: "none",
+          secure: true,
+        })
+        .status(201)
+        .json(details);
+    } else {
+      next(createError(400, "Invalid user data!"));
+    }
   } catch (error) {
     next(error);
   }
@@ -49,12 +69,40 @@ export const login = async (req, res, next) => {
 
     const isPassCorrect = await bcrypt.compare(password, user.password);
 
+    const token = generateToken(user?._id);
+
     if (isPassCorrect) {
+      res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        sameSite: "none",
+        secure: true,
+      });
+    }
+
+    if (user && isPassCorrect) {
       const { password, ...details } = user._doc;
       res.status(200).json(details);
     } else {
       return next(createError(400, "Invalid credentials!"));
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = async (req, res, next) => {
+  try {
+    res.cookie("token", "", {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: "none",
+      secure: true,
+    });
+
+    res.status(200).json({ message: "Logged out successfully!" });
   } catch (error) {
     next(error);
   }
