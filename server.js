d14ad0389c60c090fba7c172f3a086e46c2bd4b0
Aman <amangarg0303@gmail.com>
2024-04-27 00:14:14 +0530

    fix cors

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ const port = process.env.PORT || 8000;
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://swiptory-frontend-three.vercel.app/",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
 
 // mongodb connection
 mongoose
